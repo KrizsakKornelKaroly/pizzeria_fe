0c0d2c99b3140ba27b045904d322944413535455
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2025-11-09 23:05:06 +0100

    profile update, not working wo. debugging

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../services/api.service';
+import { User } from '../../../interfaces/user';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
     constructor(
-      private api: ApiService
+      private api: ApiService,
+      private auth: AuthService
     ) { }
+
+    userData : User = {
+      id: 0,
+      name: '',
+      email: '',
+      password: '',
+      phone: '',
+      address: '',
+      role: '',
+      reg: '',
+      last: '',
+      status: false
+    };
+
+    ngOnInit(): void {
+      this.userData = this.auth.loggedUser();
+    }
 
   save() { }
 }
